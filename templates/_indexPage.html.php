037188dfe259d7fd549110385af441b756cf3ae3
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-03-12 18:13:33 +1100

    Initial commit

---
+++ templates/_indexPage.html.php
@@ -0,0 +1,2 @@
+<h1>Home page</h1>
+<p>Welcome to the wonderful Northwind website! Enjoy your time with us...⌛</p>
---
+++ templates/_layout.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'NO TITLE' ?> - Northwind</title>
+</head>
+<body>
+    <?= $content ?? 'No Content: $content is not defined' ?>
+    
+</body>
+</html>
