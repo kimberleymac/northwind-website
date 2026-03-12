74c0f97dc5083790dc6c909ea7c99c465e16ddc1
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-03-12 21:07:38 +1100

    addded about.php

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php
+//Setup for the page
+//Config
+$title = "About our company";
+
+// Start output buffering (trap the output instead of displaying it)
+ob_start();
+
+// Include the page-specific template/content
+include_once "templates/_about.html.php";
+
+// Stop output buffering (store output in $content variable)
+$content = ob_get_clean();
+
+//Test
+//$content = 'howdy partner';
+
+// Include the main layout template (with custom $content)
+//include __DIR__ . "templates/_layout.html.php";
+include_once "templates/_layout.html.php";
+
+// adding some comment in here
