748b2875b670b6354fabb30e8cab317b0de845a7
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-03-12 20:57:05 +1100

    added semantic html

--- templates/_layout.html.php
+++ templates/_layout.html.php
@@ -4,14 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'NO TITLE' ?> - Northwind</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    <?= $content ?? 'No Content: $content is not defined' ?>
-    <div class="random-content">
-        <h1>Random content for fun!</h1>
-        <p>I have no idea why this content is here... it's totally random. 🤔</p>
-        <p>another change</p>
+    <div class="site-wrapper">
+        <header class="site-header">
+            <h1 class="site-title">Northwind Website</h1>
+                <div class="main-nav-container">
+                    <nav class="main-nav">
+                        <!-- Links go here -->
+                    </nav>
+                </div>
+            
+        </header>
+
+        <main class="main-content">
+    
+        <?= $content ?? 'No Content: $content is not defined' ?>
 
     </div>
+</main>
+
+<footer class="site-footer">
+<p class="copyright">Copyright 2026 Northwind</p>
+</footer>
+
+</div>
+
 </body>
 </html>
