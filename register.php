8506b8e4d42e52fd321e7f13b2b7e545e9c87ea9
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-03-26 18:13:28 +1100

    Added error validation

--- register.php
+++ register.php
@@ -1,5 +1,10 @@
 <?php
 //Setup for the page
+
+// Common includes for main PHP pages (controllers)
+require_once "includes/common.php";
+
+
 //Config
 $title = "About our company";
 
@@ -24,21 +29,51 @@ if(isset($_POST['submitRegister'])){
     $firstName = $_POST["firstName"] ?? "";
     $lastName = $_POST["lastName"] ?? "";
     $email = $_POST["email"] ?? "";
+    $password1 = $_POST["password1"] ?? "";
+    $password2 = $_POST["password2"] ?? "";
     $course = $_POST["course"] ?? "";
+    $enrolmentMode = $_POST["enrolmentMode"] ?? "";$newsletterChecked = isset($_POST["newsletter"]);
     $comments = $_POST["comments"] ?? "";
-    $enrolmentMode = $_POST["enrolmentMode"] ?? "";
-    $newsletterChecked = isset($_POST["newsletter"]);
     
 
 
     // TODO: Normalise/sanitise data
     $firstName = trim($firstName);
 
-    // TODO: Validate fields
+    // TODO: Validate fields....
+
+    // Validate first name
     if ($firstName === ""){
-        $errors["firstName"] = "Firstname is required";
-    } else if (strlen($firstName) < 2 ){
-        $errors['firstName'] = 'Firstname must be 2+ characters';
+        $errors["firstName"] = "First name is required";
+    } else if (strlen($firstName) < 2 || strlen($lastName) > 50  ){
+        $errors['firstName'] = 'First name must be between 2-50 characters';
+
+    }
+
+    // Validate last name
+    if ($lastName === ""){
+        $errors["lastName"] = "Last name is required";
+    } else if (strlen($lastName) < 2 || strlen($lastName) > 50 ){
+        $errors['lastName'] = 'Last name must be between 2-50 characters';
+
+    }
+
+    //Validate email
+    if ($email === ""){
+        $errors["email"] = "Email is required";
+    } 
+    // Email pattern match
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = 'Invalid email address';
+
+    }
+
+
+    // Validate passwords
+    if (strlen($password1) < 10){
+        $errors["password1"] = "Password must be 10+ characters";
+    } else if ($password2 !== $password1) {
+        $errors["password2"] = "Passwords don't match";
 
     }
 
